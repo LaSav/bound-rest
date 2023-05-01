b5ae4d56ff1854a272e47f2c8d139dc8c2bebb55
savva <savva.lambin@gmail.com>
2023-05-01 21:32:11 +0100

    Add express async handler

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -1,32 +1,33 @@
+const asyncHandler = require('express-async-handler');
 // @desc Get Listings
 // @route GET /api/listings
 // @access Private
-const getListings = (req, res) => {
+const getListings = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Listings' });
-};
+});
 // @desc Create a Listing
 // @route POST /api/listings
 // @access Private
-const createListing = (req, res) => {
+const createListing = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please Add Text Field');
   }
   console.log(req.body);
   res.status(200).json({ message: 'Create Listing' });
-};
+});
 // @desc Update a Listing
 // @route /api/listings/:id
 // @access Private
-const updateListing = (req, res) => {
+const updateListing = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Listing ${req.params.id}` });
-};
+});
 // @desc Delete a Listing
 // @route /api/listings/:id
 // @access Private
-const deleteListing = (req, res) => {
+const deleteListing = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Listing ${req.params.id}` });
-};
+});
 module.exports = {
   getListings,
   createListing,
