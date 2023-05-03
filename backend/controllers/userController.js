4a29f8f634854c890bd5ab85bb8d81372dac3f5a
savva <savva.lambin@gmail.com>
2023-05-03 15:10:29 +0100

    Add JWT authentication for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,9 +35,10 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (user) {
     res.status(201).json({
-      _id: user.id,
+      _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -61,6 +62,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -70,9 +72,21 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get User data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: 'User data display' });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = { registerUser, loginUser, getMe };
