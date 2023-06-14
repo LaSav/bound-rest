a1a5d916e79ac1c18ccc5baea82a1b5de55c211f
savva <savva.lambin@gmail.com>
2023-06-14 12:27:24 +0100

    Add Provider with store in root index for redux

--- frontend/src/store/apis/usersApi.js
+++
@@ -1,4 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-// get user from localstorage
-const user = JSON.parse(localStorage.getItem('user'));
--- frontend/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/dist/query';
-
-export const store = configureStore({});
-
-setupListeners(store.dispatch);
